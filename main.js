93e63fb602a7a9b0a34d6e8a22e805b351247f5d
zexunT <921620799@qq.com>
2020-07-31 11:07:58 +0800

    second commit

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 var wrapper = document.getElementsByClassName('wrapper')[0]
-var items = document.getElementsByClassName('item')
+var items = document.querySelectorAll('.item')
+var slots = document.querySelectorAll(".slot")
 var leftBtn = document.getElementById('left')
 var rightBtn = document.getElementById('right')
-var slots = document.getElementsByClassName("slot")
 var index = 0
-// console.log(items[0])
 //初始化重置每张图片的class值
 var initClass = function () {
-    for (var i = 0; i < items.length; i++) {
-        items[i].className = 'item';
-    }
-    for (var i = 0; i < slots.length; i++) {
-        slots[i].className = 'slot';
-    }
+    items.forEach(x => x.className='item')
+    slots.forEach(x => x.className='slot')
 }
 //根据索引值，显示图片
 var renderIndex = function (index) {
     //先将所有类名重置，再根据index添加类名active让其显示
     initClass();
-    items[index].className = 'item active' //渲染图片
-    slots[index].className = 'slot active';//渲染小黑点
+    items[index].classList.add('active') //渲染图片
+    slots[index].classList.add('active');//渲染小黑点
 }
 //点击左键，右键，改变索引值进行渲染
 leftBtn.onclick = function () {
@@ -47,18 +42,19 @@ for (var i = 0; i < slots.length; i++) {
 //自动向右切换图片
 var autoRender = function () {
     index++
-    console.log(index)
+    // console.log(index)
     if (index < items.length) {
     } else {
         index = 0
     }
     renderIndex(index)
 }
-var timer = setInterval(autoRender, 3000)
+var timer = setInterval(autoRender, 5000)
 //当鼠标放置上去时，清除定时器
 wrapper.onmouseover = function () {
     clearInterval(timer);
 }
+//当鼠标离开时，重启定时器
 wrapper.onmouseout = function () {
-    timer = setInterval(autoRender, 3000)
+    timer = setInterval(autoRender, 5000)
 }
